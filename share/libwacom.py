183a63254789d85c2b1a426354a16fafe67d5743
Marusoftware <marusoftware@outlook.jp>
2020-10-23 01:20:54 +0900

    clean

--- share/libwacom.py
+++
@@ -1,8 +0,0 @@
-######################################
-#Marusoftware LibWacom python wrapper#
-######################################
-import ctypes
-import ctypes.util
-import os
-
-libwacom = ctypes.cdll.LoadLibrary(ctypes.util.find_library("wacom"))

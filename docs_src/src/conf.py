3e0aaf0988ce0daf4a1f37a2f6a94bb43223473f
Maruo.S <raspi-maru2004@outlook.jp>
2022-03-23 23:11:58 +0900

    add some modues to doc

--- docs_src/src/conf.py
+++ docs_src/src/conf.py
@@ -34,7 +34,9 @@ release = 'b1.0.0'
 import sphinx_rtd_theme
 extensions = [
     "sphinx_rtd_theme",
-    "sphinx.ext.githubpages"
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx_multiversion"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -64,3 +66,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}

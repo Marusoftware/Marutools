2f27b7c485a4b5107e8dd164fcf653af6c8f4608
Maruo.S <raspi-maru2004@outlook.jp>
2022-03-25 00:19:16 +0900

    change docs to multiversion

--- docs_src/src/conf.py
+++ docs_src/src/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -67,3 +67,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+smv_tag_whitelist = r'^.\d+\.\d+\.\d+$'
+smv_branch_whitelist = r'^master$'
+smv_remote_whitelist = r'^.*$'

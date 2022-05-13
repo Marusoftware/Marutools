d85b8576be7585aa77ec845774fcc32fdd28d6a6
Maruo.S <raspi-maru2004@outlook.jp>
2022-05-13 10:30:27 +0900

    delete libtools's unused funcs

--- libtools/__init__.py
+++ libtools/__init__.py
@@ -1,5 +1,3 @@
-from .data import Config, Lang
 from .addon import Addon
-from . import core, exception, ui as UI
 
-__all__ = ["Config", "Lang", "core", "exception", "UI", "Addon"]
+__all__ = ["Addon"]
--- libtools/ui/GUI_tkinter/wacom.py
+++
@@ -1,1 +0,0 @@
-#reWriting

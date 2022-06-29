8980e014b87a785c800e6964173b35bf7a101276
Maruo.S <raspi-maru2004@outlook.jp>
2022-06-29 10:26:32 +0900

    add graph addon(empty)

---
+++ addons/graph/__init__.py
@@ -0,0 +1,14 @@
+class Edit():
+    name="Graph"
+    description="Graph editor"
+    file_types=["mplot"]
+    def __init__(self, api):
+        self.api=api
+    def on_modify(self):
+        pass
+    def save(self, file=None):
+        pass
+    def new(self):
+        pass
+    def close(self):
+        pass
